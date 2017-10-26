10f230bd482a305c9fb90c1f15f0d3994f0b716e
Piotrek <@>
2017-10-26 12:14:20 +0200

    przesłanie plików

---
+++ A_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadź ciąg znaków i naciśnij enter.");
+            string abc = Console.ReadLine();
+            Console.WriteLine(abc.ToLower());
+            Console.ReadKey();
+        }
+    }
+}
